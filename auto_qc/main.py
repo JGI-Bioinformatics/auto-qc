2cb1a78e9e234c058634612b6ccb96c5bc03d58e
Michael Barton <mail@michaelbarton.me.uk>
2014-05-12 11:04:26 -0700

    Split node evaluation into separate method
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- auto_qc/main.py
+++ auto_qc/main.py
@@ -1,17 +1,11 @@
 import auto_qc.util.file_system as fs
 import auto_qc.util.workflow    as flow
+import auto_qc.util.workflow    as flow
 import operator                 as op
 import functools                as ft
 import yaml
 
 from fn import iters as it
-
-method_chain = [
-    (fs.check_for_file, ['analysis_file']),
-    (fs.check_for_file, ['threshold_file']),
-    (fs.read_yaml_file, ['threshold_file', 'thresholds']),
-    (fs.read_yaml_file, ['analysis_file',  'analyses']),
-        ]
 
 OPERATORS = {
     'greater_than': op.gt,
@@ -30,16 +24,28 @@ def find_analysis_value(analyses, namespace, path):
     outputs = analysis['outputs']
     return reduce(lambda k, v: k[v], path.split('/'), outputs)
 
+def evaluate_nodes(destination, status):
+    nodes    = status['thresholds']['thresholds']
+    analyses = status['analyses']
+    f = lambda n: [evaluate_threshold_node(analyses, n), n]
+    status[destination] = map(f, nodes)
+    return status
+
+
+
+method_chain = [
+    (fs.check_for_file, ['analysis_file']),
+    (fs.check_for_file, ['threshold_file']),
+    (fs.read_yaml_file, ['threshold_file', 'thresholds']),
+    (fs.read_yaml_file, ['analysis_file',  'analyses']),
+    (evaluate_nodes,    ['node_results']),
+        ]
+
 def run(args):
     status = flow.thread_status(method_chain, args)
     flow.exit_if_error(status)
 
-    nodes    = status['thresholds']['thresholds']
-    analyses = status['analyses']
-
-    f       = lambda n: [evaluate_threshold_node(analyses, n), n]
-    node_results = map(f, nodes)
-    failing      = map(it.head, node_results)
+    failing = map(it.head, status['node_results'])
 
     if any(failing):
         print 'FAIL'
