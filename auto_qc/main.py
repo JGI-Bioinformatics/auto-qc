e5271ad9389b103cace0b9c33074ee0c7c7ff45c
Michael Barton <mail@michaelbarton.me.uk>
2014-05-11 21:39:52 -0700

    Implement node evaluation
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- auto_qc/main.py
+++ auto_qc/main.py
@@ -1,10 +1,27 @@
 import auto_qc.util.file_system as fs
 import auto_qc.util.workflow    as flow
+import operator                 as op
 
 method_chain = [
     (fs.check_for_file, ['analysis_file']),
     (fs.check_for_file, ['threshold_file']),
         ]
+
+OPERATORS = {
+    'greater_than': op.gt,
+        }
+
+def evaluate_threshold_node(node, analyses):
+    path, threshold = node['args']
+    namespace       = node['analysis']
+    value           = find_analysis_value(analyses, namespace, path)
+    f               = OPERATORS[node['operator']]
+    return f(value, threshold)
+
+def find_analysis_value(analyses, namespace, path):
+    analysis = filter(lambda x: x['analysis'] == namespace, analyses)[0]
+    outputs = analysis['outputs']
+    return reduce(lambda k, v: k[v], path.split('/'), outputs)
 
 def run(args):
     status = flow.thread_status(method_chain, args)
