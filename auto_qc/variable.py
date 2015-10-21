d64af9e15abbb5fe5846734c1b07317cce1ee275
Michael Barton <mail@michaelbarton.me.uk>
2015-10-21 16:44:12 -0400

    Refactor variable module to remove namespaces

--- auto_qc/variable.py
+++ auto_qc/variable.py
@@ -6,19 +6,20 @@ def is_variable(var):
     """
     return isinstance(var, basestring) and it.head(var) == ':'
 
-def split_into_namespace_and_path(variable_string):
-    drop_colon = variable_string[1:]
+def is_variable_path_valid(analysis, path):
+    """
+    Does the variable path have a matching value in the analysis?
+    """
+    try:
+        get_variable_value(analysis, path)
+    except KeyError as e:
+        return False
+    return True
+
+def get_variable_value(analysis, path):
+    """
+    Get variable value by traversing path into analysis
+    """
+    drop_colon = path[1:]
     path_array = drop_colon.split('/')
-    return [it.head(path_array), it.tail(path_array)]
-
-def get_analysis(analyses, var):
-    namespace, _ = split_into_namespace_and_path(var)
-    return it.head(filter(lambda x: x['analysis'] == namespace, analyses))
-
-def get_variable_value(analysis, var):
-    _, path = split_into_namespace_and_path(var)
-    return reduce(lambda a, k: a[k], path, analysis['outputs'])
-
-def variable(analyses, path):
-    analysis = get_analysis(analyses, path)
-    return get_variable_value(analysis, path)
+    return reduce(lambda a, k: a[k], path_array, analysis['data'])
