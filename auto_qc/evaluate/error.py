a60c675ee08198a46aeda741b7f8037415072e14
Michael Barton <mail@michaelbarton.me.uk>
2016-06-08 15:11:13 -0600

    Refactor function for testing whether node operators are valid

--- auto_qc/evaluate/error.py
+++ auto_qc/evaluate/error.py
@@ -9,6 +9,21 @@ import auto_qc.variable        as var
 import auto_qc.node            as nd
 import auto_qc.util.functional as fn
 
+
+def variable_error_message(variable):
+    msg = "No matching metric '{}' found."
+    return msg.format(variable)
+
+
+def operator_error_message(operator):
+    msg = "Unknown operator '{}.'"
+    return msg.format(operator)
+
+
+def generator_error_string(f, xs):
+    return st.join(map(f, xs), "\n")
+
+
 def check_version_number(threshold, status):
     version =  meta.major_version()
     threshold_version = str(status[threshold]['metadata']['version']['auto-qc'])
@@ -21,47 +36,31 @@ Please update the syntax to version >= {}.0.0.
 
     return status
 
-def variable_error_message(variable):
-    msg = "No matching metric '{}' found."
-    return msg.format(variable)
-
 
 def check_node_paths(nodes, analyses, status):
     """
-    Checks all variable paths listed in the QC file are valid. Sets an error
+    Checks that all variable paths listed in the QC file are valid. Sets an error
     message in the status if not.
     """
     variables = var.get_variable_names(status[nodes]['thresholds'])
     f = funcy.partial(var.is_variable_path_valid, status[analyses])
-    invalid_variables = set(funcy.remove(f, variables))
+    errors = set(funcy.remove(f, variables))
 
-    if len(invalid_variables) > 0:
-        errors = map(variable_error_message, invalid_variables)
-        status['error'] = st.join(errors, "\n")
+    if len(errors) > 0:
+        status['error'] = generator_error_string(variable_error_message, errors)
 
     return status
 
 
 def check_operators(node_ref, status):
-    nodes = status[node_ref]['thresholds']
+    """
+    Checks that all operators listed in the QC file are valid. Sets an error
+    message in the status if not.
+    """
+    operators = funcy.mapcat(nd.get_all_operators, status[node_ref]['thresholds'])
+    errors    = funcy.remove(nd.is_operator, operators)
 
-    def _f(x):
-      return map(fn.recursive_apply(_parse_list, fn.empty_list), x)
-
-    def _parse_list(node):
-        if isinstance(it.head(node), dict):
-            return _parse_list(list(it.tail(node)))
-        else:
-            operator = it.head(node)
-            rest     = it.tail(node)
-            return [operator] + _f(rest)
-
-    operators = fn.flatten(_f(nodes))
-
-    f = F(it.map, lambda x: "Unknown operator '{}.'".format(x)) << F(it.filterfalse, nd.is_operator)
-
-    errors = list(f(operators))
     if len(errors) > 0:
-        status['error'] = st.join(errors, "\n")
+        status['error'] = generator_error_string(operator_error_message, errors)
 
     return status
